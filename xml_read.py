03d8c585391fd27ac1dc9d7ec55913c974e6ba7e
Verus <verusscr@gmail.com>
2018-10-02 20:15:45 +0200

    A stable version of the Highlight

--- xml_read.py
+++ xml_read.py
@@ -4,25 +4,24 @@ import api
 
 xmldata = []
 
-
 def read_keywords_from_xml(self):
-    way = api.find_way_to_file(self, "keywords.xml")
+    way = api.find_way_to_file(self,"keywords.xml")
     xml_doc = minidom.parse(way)
 
-    self.xmldata.append([])
+    xmldata.append([])
     # While to takes elements from xml file
     for item in xml_doc.getElementsByTagName('keywords')[0].getElementsByTagName("item"):
-        self.xmldata[0].append(item.attributes["word"].value)
-    self.xmldata.append([])
+        xmldata[0].append(item.attributes["word"].value)
+    xmldata.append([])
     for item in xml_doc.getElementsByTagName('opertors')[0].getElementsByTagName("item"):
-        self.xmldata[1].append(item.attributes["symbol"].value)
-    self.xmldata.append([])
+        xmldata[1].append(item.attributes["symbol"].value)
+    xmldata.append([])
     for item in xml_doc.getElementsByTagName('comment')[0].getElementsByTagName("item"):
-        self.xmldata[2].append(item.attributes["symbol"].value)
-    self.xmldata.append([])
+        xmldata[2].append(item.attributes["symbol"].value)
+    xmldata.append([])
     for item in xml_doc.getElementsByTagName('braces')[0].getElementsByTagName("item"):
-        self.xmldata[3].append(item.attributes["key"].value)
-    self.xmldata.append([])
+        xmldata[3].append(item.attributes["key"].value)
+    xmldata.append([])
     for item in xml_doc.getElementsByTagName('function')[0].getElementsByTagName("item"):
-        self.xmldata[4].append(item.attributes["func"].value)
-    self.xmldata.append([])
+        xmldata[4].append(item.attributes["func"].value)
+    xmldata.append([])
