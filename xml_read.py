576c01693d20306f954a47fcf28033e166b014a9
Verus <verusscr@gmail.com>
2018-10-03 20:00:11 +0200

    Code after refactoring

--- xml_read.py
+++ xml_read.py
@@ -5,7 +5,7 @@ import api
 xmldata = []
 
 def read_keywords_from_xml(self):
-    way = api.find_way_to_file(self,"keywords.xml")
+    way = api.find_way_to_file("keywords.xml")
     xml_doc = minidom.parse(way)
 
     xmldata.append([])
@@ -16,12 +16,6 @@ def read_keywords_from_xml(self):
     for item in xml_doc.getElementsByTagName('opertors')[0].getElementsByTagName("item"):
         xmldata[1].append(item.attributes["symbol"].value)
     xmldata.append([])
-    for item in xml_doc.getElementsByTagName('comment')[0].getElementsByTagName("item"):
-        xmldata[2].append(item.attributes["symbol"].value)
-    xmldata.append([])
-    for item in xml_doc.getElementsByTagName('braces')[0].getElementsByTagName("item"):
-        xmldata[3].append(item.attributes["key"].value)
-    xmldata.append([])
     for item in xml_doc.getElementsByTagName('function')[0].getElementsByTagName("item"):
-        xmldata[4].append(item.attributes["func"].value)
+        xmldata[2].append(item.attributes["func"].value)
     xmldata.append([])
